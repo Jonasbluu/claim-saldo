d7f4977fc802bd1ad2698716bffacdb017eb187f
Jonasbluu <jblu3541@gmail.com>
2026-06-26 02:45:38 +0700

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 const envelope = document.getElementById("envelope");
 
 envelope.addEventListener("click", () => {
-
     envelope.classList.toggle("open");
-
-});
-
-document.querySelector(".claim-btn")
-.addEventListener("click", () => {
-
-    alert("Hadiah berhasil diklaim!");
-
-    // redirect jika diperlukan
-    // window.location.href="https://linkanda.com";
-
 });
