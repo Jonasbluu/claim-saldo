adc13ba640cf95a6b5053f5ef33ffca3621bc545
Jonasbluu <jblu3541@gmail.com>
2026-06-26 02:57:06 +0700

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const envelope = document.getElementById("envelope");
+const popup = document.getElementById("popupLetter");
 
-envelope.addEventListener("click", () => {
-    envelope.classList.toggle("open");
+const hadiah = [
+    "Rp18.000",
+    "Rp28.000",
+    "Rp38.000",
+    "Rp58.000",
+    "Rp88.000",
+    "Rp188.000"
+];
+
+envelope.addEventListener("click",()=>{
+
+    const random =
+    hadiah[Math.floor(Math.random()*hadiah.length)];
+
+    document.getElementById("reward").innerText =
+    random;
+
+    popup.classList.add("show");
+
 });
